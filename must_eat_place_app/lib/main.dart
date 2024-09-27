2f2394341ae95f49a5b75e1442727875f995d307
LrZl <dingtti@naver.com>
2024-09-27 11:22:34 +0900

    SplashScreen
    add

--- must_eat_place_app/lib/main.dart
+++ must_eat_place_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:must_eat_place_app/view/login/login.dart';
+import 'package:must_eat_place_app/view/splashScrrenPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Login(),
+      home: const SplashScreen(),
     );
   }
 }
